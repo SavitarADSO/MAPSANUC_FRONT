b72502dd78d6317ba92c6780b0323829713d46ce
SavitarADSO <pee7491@gmail.com>
2024-06-05 19:40:15 -0500

    Como llegar

--- src/app/mapa/anuc/anuc.component.ts
+++ src/app/mapa/anuc/anuc.component.ts
@@ -1,33 +1,36 @@
 import { Component, AfterViewInit } from '@angular/core';
-import { Map, marker, tileLayer, Icon, LatLng } from 'leaflet';
+import { Map, marker, tileLayer, Icon, LatLng, Control } from 'leaflet';
+import * as L from 'leaflet';
+import 'leaflet-routing-machine';
 
 @Component({
   selector: 'app-anuc',
   standalone: true,
-  imports: [],
   templateUrl: './anuc.component.html',
   styleUrls: ['./anuc.component.css']
 })
 export class AnucComponent implements AfterViewInit {
   private initialView: LatLng = new LatLng(7.06260, -73.08583);
   private initialZoom: number = 17;
-  private popupHeight: number = 200; // Ajustar esto según la altura esperada del popup
+  private popupHeight: number = 200;
+  private map!: Map;
+  private routeControl!: Control;
+  private userMarker!: L.Marker;
+  private anucMarker!: L.Marker;
 
   ngAfterViewInit(): void {
-    const map = new Map('map').setView(this.initialView, this.initialZoom);
+    this.map = new Map('map').setView(this.initialView, this.initialZoom);
     tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 19,
-    }).addTo(map);
+    }).addTo(this.map);
 
-    // Definir el icono personalizado
     const customIcon = new Icon({
       iconUrl: 'assets/img/mi_finca.png',
-      iconSize: [38, 38], // Tamaño del icono (ajustar según sea necesario)
-      iconAnchor: [19, 38], // Punto del icono que corresponderá a la posición del marcador (ajustar según sea necesario)
-      popupAnchor: [0, -38] // Punto desde el cual se abrirá el popup relativo al icono (ajustar según sea necesario)
+      iconSize: [38, 38],
+      iconAnchor: [19, 38],
+      popupAnchor: [0, -38]
     });
 
-    // Contenido del popup con imágenes y texto
     const popupContent = `
     <div style="text-align: center;">
       <div style="display: flex; align-items: center; justify-content: center; margin-bottom: 10px;">
@@ -42,35 +45,106 @@ export class AnucComponent implements AfterViewInit {
         <li>Sábado: 8:00 am - 4:00 pm</li>
         <li>Domingo: 8:00 am - 4:00 pm</li>
       </ul>
-      <img src="assets/img/fachada_anuc.PNG" alt="Fachada ANUC" style="width: 200px; height: auto; margin-top: 10px;">
+      <img src="assets/img/fachada_anuc.PNG" alt="Fachada ANUC" style="width: 200px; height: auto; margin-top: 10px;"><br>
     </div>
   `;
 
-    // Crear el marcador con el icono personalizado y el contenido del popup
-    const markerItem = marker([7.062166379405107, -73.08538016671284], { icon: customIcon })
-      .addTo(map)
-      .bindPopup(popupContent);
+    // Crear marcador de ANUC
+    this.anucMarker = marker([7.062166379405107, -73.08538016671284], { icon: customIcon })
+      .addTo(this.map)
+      .bindPopup(popupContent)
+      .openPopup();
 
-    // Abrir el popup automáticamente y ajustar la vista del mapa
-    markerItem.openPopup();
-    this.adjustPopupView(map, markerItem);
+    this.adjustPopupView(this.map, this.anucMarker);
 
-    // Ajustar la vista del mapa para mostrar completamente el popup cuando se abre
-    markerItem.on('popupopen', () => {
-      this.adjustPopupView(map, markerItem);
-    });
-
-    // Restablecer la vista del mapa y abrir el popup cuando se hace clic en el marcador
-    markerItem.on('click', () => {
-      map.setView(this.initialView, this.initialZoom);
-      this.adjustPopupView(map, markerItem);
-      markerItem.openPopup();
+    // Ajustar vista al hacer clic en el marcador de ANUC
+    this.anucMarker.on('click', () => {
+      this.map.setView(this.initialView, this.initialZoom);
+      this.adjustPopupView(this.map, this.anucMarker);
+      this.anucMarker.openPopup();
     });
   }
 
   private adjustPopupView(map: Map, markerItem: any): void {
     setTimeout(() => {
       map.panBy([0, -this.popupHeight / 2]);
-    }, 300); // Ajuste de tiempo para asegurar que el popup esté completamente cargado
+    }, 300);
   }
+
+  startNavigation(): void {
+    console.log('Navigate button clicked');
+
+    if (!this.anucMarker) {
+      console.error('Marker for ANUC not found.');
+      return;
+    }
+
+    const destination = this.anucMarker.getLatLng();
+    this.navigate(destination);
+  }
+
+  private navigate(destination: LatLng): void {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const userLocation = new LatLng(position.coords.latitude, position.coords.longitude);
+  
+          if (this.userMarker) {
+            this.map.removeLayer(this.userMarker);
+          }
+  
+          // Cambiar el icono del marcador del usuario
+          const userIcon = new Icon({
+            iconUrl: 'assets/img/memap.png',
+            iconSize: [38, 38],
+            iconAnchor: [19, 38],
+            popupAnchor: [0, -38]
+          });
+  
+          // Crear marcador de usuario
+          this.userMarker = marker(userLocation, { icon: userIcon }).addTo(this.map).bindPopup('Tú estás aquí');
+          this.userMarker.openPopup();
+  
+          if (this.routeControl) {
+            this.map.removeControl(this.routeControl);
+          }
+  
+          // Crear control de enrutamiento hacia el marcador de ANUC
+          this.routeControl = (L as any).Routing.control({
+            waypoints: [userLocation, destination],
+            routeWhileDragging: true,
+            showAlternatives: true,
+            altLineOptions: {
+              styles: [{ color: 'black', opacity: 0.15, weight: 9 }]
+            },
+            createMarker: (i: number, wp: any, nWps: number) => {
+              if (i === 0) {
+                return marker(wp.latLng, { icon: userIcon }).bindPopup('Tú estás aquí');
+              } else if (i === nWps - 1) {
+                const anucMarker = this.anucMarker;
+                if (anucMarker && anucMarker.getPopup()) {
+                  const popupContent = anucMarker.getPopup()?.getContent();
+                  if (popupContent) {
+                    return marker(wp.latLng, { icon: anucMarker.options.icon }).bindPopup(popupContent);
+                  }
+                }
+                return marker(wp.latLng);
+              } else {
+                return marker(wp.latLng);
+              }
+            }
+          }).addTo(this.map);
+  
+          this.map.setView(userLocation, this.initialZoom);
+        },
+        (error) => {
+          alert('Error al obtener la ubicación: ' + error.message);
+        }
+      );
+    } else {
+      alert('Geolocalización no es soportada por este navegador.');
+    }
+  }
+  
+  
 }
--- src/app/mapa/asociado/asociado.component.ts
+++ src/app/mapa/asociado/asociado.component.ts
@@ -1,9 +1,10 @@
 import { Component, AfterViewInit, ElementRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { UbicacionService } from '../../services/ubicacion.service';
 import { Ubicacion } from '../../models/ubicacion.model';
 import { HttpClientModule } from '@angular/common/http';
-import { Map, tileLayer, Icon } from 'leaflet';
+import { Map, tileLayer, Icon, marker, polygon, LatLngExpression } from 'leaflet';
 import * as L from 'leaflet'; // Importamos 'L' de Leaflet
 import 'leaflet-routing-machine';
 import 'leaflet-routing-machine/dist/leaflet-routing-machine.css'; // Asegúrate de importar el CSS si aún no lo has hecho
@@ -11,13 +12,19 @@ import 'leaflet-routing-machine/dist/leaflet-routing-machine.css'; // Asegúrate
 @Component({
   selector: 'app-asociado',
   standalone: true,
-  imports: [CommonModule, HttpClientModule], // Añadir HttpClientModule aquí
+  imports: [CommonModule, HttpClientModule, FormsModule], // Añadir HttpClientModule aquí
   templateUrl: './asociado.component.html',
   styleUrls: ['./asociado.component.css'],
   providers: [UbicacionService]
 })
 export class AsociadoComponent implements AfterViewInit {
   private map: Map | undefined;
+  private markers: any[] = [];
+  public veredas: string[] = [];
+  public tiposFinca: string[] = [];
+  public categorias: string[] = [];
+  public productos: string[] = [];
+  public hectareas: number[] = [];
 
   constructor(private elementRef: ElementRef, private ubicacionService: UbicacionService) {}
 
@@ -30,7 +37,7 @@ export class AsociadoComponent implements AfterViewInit {
 
   private initializeMap(): Promise<void> {
     return new Promise<void>((resolve, reject) => {
-      this.map = new Map('map').setView([7.0672,-73.0755], 13);
+      this.map = new Map('map').setView([7.06260, -73.08583], 13);
       tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
       }).addTo(this.map);
@@ -107,22 +114,63 @@ export class AsociadoComponent implements AfterViewInit {
   }
 
   private loadUbicaciones(): void {
-    // Definir el icono personalizado para las demás fincas
     const fincaIcon = new Icon({
       iconUrl: 'assets/img/fincasots.png',
-      iconSize: [38, 38], // Ajusta el tamaño según sea necesario
-      iconAnchor: [19, 38], // Punto del icono que corresponderá a la posición del marcador
-      popupAnchor: [0, -38] // Punto desde el cual se abrirá el popup relativo al icono
+      iconSize: [38, 38],
+      iconAnchor: [19, 38],
+      popupAnchor: [0, -38]
     });
 
     this.ubicacionService.getUbicaciones().subscribe((ubicaciones: Ubicacion[]) => {
-      ubicaciones.forEach((ubicacion: Ubicacion) => {
+      const fincas = ubicaciones.slice(0, 10);
+
+      fincas.forEach((ubicacion: Ubicacion) => {
         if (ubicacion.latitude && ubicacion.longitude) {
           const lat = parseFloat(ubicacion.latitude);
           const lng = parseFloat(ubicacion.longitude);
-          L.marker([lat, lng], { icon: fincaIcon })
-            .addTo(this.map!)
-            .bindPopup(`Finca: ${ubicacion.nombre_finca}<br>Vereda: ${ubicacion.vereda}<br>Nombre: ${ubicacion.nombre}`);
+
+          const markerItem = marker([lat, lng], { icon: fincaIcon })
+            .bindPopup(`
+              <div>
+                <p><strong>Finca:</strong> ${ubicacion.nombre_finca}</p>
+                <p><strong>Vereda:</strong> ${ubicacion.vereda}</p>
+                <p><strong>Nombre:</strong> ${ubicacion.nombre}</p>
+                <p><strong>Tipo de Finca:</strong> ${ubicacion.tipo_finca}</p>
+                <p><strong>Hectáreas:</strong> ${ubicacion.hectareas}</p>
+                <p><strong>Producción:</strong></p>
+                <ul>
+                  ${ubicacion.producciones?.map(prod => `<li>${prod.categorias}: ${prod.productos}</li>`).join('')}
+                </ul>
+              </div>
+            `)
+            .addTo(this.map!);
+
+          this.markers.push({
+            marker: markerItem,
+            vereda: ubicacion.vereda,
+            tipoFinca: ubicacion.tipo_finca,
+            categoria: ubicacion.producciones?.map(prod => prod.categorias).join(', ') || '',
+            producto: ubicacion.producciones?.map(prod => prod.productos).join(', ') || '',
+            hectareas: ubicacion.hectareas || 0
+          });
+
+          if (ubicacion.vereda && !this.veredas.includes(ubicacion.vereda)) {
+            this.veredas.push(ubicacion.vereda);
+          }
+          if (ubicacion.tipo_finca && !this.tiposFinca.includes(ubicacion.tipo_finca)) {
+            this.tiposFinca.push(ubicacion.tipo_finca);
+          }
+          ubicacion.producciones?.forEach(prod => {
+            if (prod.categorias && !this.categorias.includes(prod.categorias)) {
+              this.categorias.push(prod.categorias);
+            }
+            if (prod.productos && !this.productos.includes(prod.productos)) {
+              this.productos.push(prod.productos);
+            }
+          });
+          if (ubicacion.hectareas && !this.hectareas.includes(ubicacion.hectareas)) {
+            this.hectareas.push(ubicacion.hectareas);
+          }
         }
       });
     });
